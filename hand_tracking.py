7556d4bf3cbc61f1435e8a71b3536f0a334b5e16
PythiaUF <125420118+PythiaUF@users.noreply.github.com>
2026-02-15 11:06:02 -0500

    feat: add example overlay

--- hand_tracking.py
+++ hand_tracking.py
@@ -18,6 +18,17 @@ while True:
             for hand_landmarks in result.multi_hand_landmarks:
                 print(hand_landmarks)
                 mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        # to add translucent elements onto the frame, we need to blend a frame with a copy of itself that contains the translucent elements
+        # we can use the cv2.addWeighted() function to blend the two frames together
+        # these elements are just examples, you can add any cv2 functions on the overlay frame
+        overlay = frame.copy()
+        overlay_alpha = 0.5
+
+        cv2.rectangle(overlay, (420, 205), (595, 385), (0, 0, 255), -1)
+        cv2.putText(overlay, f"Text example, alpha={overlay_alpha}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
+        cv2.addWeighted(overlay, overlay_alpha, frame, 1 - overlay_alpha, 0, frame)
+
         cv2.imshow("capture image", frame)
         if cv2.waitKey(1) == ord('q'):
             break
